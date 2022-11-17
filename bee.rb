a83feaadc252e040271600e44248d84324e892d2
itsb27 <81706998+itsb27@users.noreply.github.com>
2022-11-17 15:53:51 -0800

    Create bee.rb

---
+++ bee.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HomebrewBee < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/itsb27/homebrew-bee/archive/refs/tags/0.0.0.tar.gz"
+  sha256 "15210cbe2601da5446835c2a1dc30ce1cb5ac0335d8009c338aa88c605ef93e8"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "bee"
+  end
+end
