9c34b7eec6f5ae7716dba87d619ec8794a5834e0
itsb27 <81706998+itsb27@users.noreply.github.com>
2022-11-17 16:04:54 -0800

    Update bee.rb

--- bee.rb
+++ bee.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class HomebrewBee < Formula
+class Bee < Formula
   desc ""
   homepage ""
   url "https://github.com/itsb27/homebrew-bee/archive/refs/tags/0.0.0.tar.gz"
